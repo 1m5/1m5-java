972c542e79512670f53c859a60affde6b022d3c0
objectorange <objectorange@1m5.io>
2019-12-23 15:06:22 -0700

    need an object to describe each peer's network status

---
+++ src/main/java/io/onemfive/network/NetworkReport.java
@@ -0,0 +1,20 @@
+package io.onemfive.network;
+
+import io.onemfive.data.JSONSerializable;
+
+import java.util.Map;
+
+public class NetworkReport implements JSONSerializable {
+
+
+
+    @Override
+    public Map<String, Object> toMap() {
+        return null;
+    }
+
+    @Override
+    public void fromMap(Map<String, Object> m) {
+
+    }
+}
