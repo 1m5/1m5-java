02562fb6d8636ecd7725dc02d4284d194e4352a4
objectorange <objectorange@1m5.io>
2019-12-23 22:21:06 -0700

    add doc

--- src/main/java/io/onemfive/network/sensors/radio/Radio.java
+++ src/main/java/io/onemfive/network/sensors/radio/Radio.java
@@ -3,9 +3,6 @@ package io.onemfive.network.sensors.radio;
 import io.onemfive.core.LifeCycle;
 import io.onemfive.network.peers.PeerReport;
 
-/**
- * Interface to use for all Radio calls.
- */
 public interface Radio extends LifeCycle {
     void setPeerReport(PeerReport peerReport);
     RadioSession establishSession(RadioPeer peer, Boolean autoConnect);
