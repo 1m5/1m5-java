5551153c63e530d6463f0b56d5adf437f6ac0cde
objectorange <objectorange@1m5.io>
2019-12-24 13:50:46 -0700

    needs to extend Request

--- src/main/java/io/onemfive/ext/bitcoin/requests/SendRequest.java
+++ src/main/java/io/onemfive/ext/bitcoin/requests/SendRequest.java
@@ -1,6 +1,8 @@
 package io.onemfive.ext.bitcoin.requests;
 
-public class SendRequest {
+import io.onemfive.data.Request;
+
+public class SendRequest extends Request {
     public String base58From;
     public String base58To;
     public long satoshis;
