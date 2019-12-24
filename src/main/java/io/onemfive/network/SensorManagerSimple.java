a8bcb1f038f69e288eccdc82a1a796fc6a455d43
objectorange <objectorange@1m5.io>
2019-12-23 22:17:11 -0700

    remove import

--- src/main/java/io/onemfive/network/SensorManagerSimple.java
+++ src/main/java/io/onemfive/network/SensorManagerSimple.java
@@ -5,6 +5,7 @@ import io.onemfive.data.Envelope;
 import io.onemfive.data.Packet;
 import io.onemfive.data.Sensitivity;
 import io.onemfive.data.route.Route;
+import io.onemfive.network.ops.NetworkOp;
 
 import java.net.URL;
 import java.util.*;
