6227d0741374bd7bdd7c72b91fcc961e72054724
objectorange <objectorange@1m5.io>
2019-12-23 22:20:05 -0700

    reachability support

--- src/main/java/io/onemfive/network/peers/PeerManager.java
+++ src/main/java/io/onemfive/network/peers/PeerManager.java
@@ -14,14 +14,28 @@ public interface PeerManager extends Runnable {
     void updateLocalPeer(AuthNRequest request);
     void updateLocalPeer(DID did);
     NetworkPeer getLocalPeer();
+    Boolean isKnown(NetworkPeer peer);
+
+    /**
+     * Is the provided toPeer reachable by the provided fromPeer?
+     * This will return false if the Network Peer is unknown or
+     * if known yet unable to reach by supplied Sensitivity level.
+     * @param fromPeer
+     * @param toPeer
+     * @param sensitivity
+     * @return
+     */
+    Boolean isReachable(NetworkPeer fromPeer, NetworkPeer toPeer, Sensitivity sensitivity);
     Boolean verifyPeer(NetworkPeer peer);
     Boolean savePeer(NetworkPeer peer, Boolean autocreate);
     NetworkPeer loadPeer(NetworkPeer peer);
     Boolean isRemoteLocal(NetworkPeer r);
     Packet buildPacket(DID origination, DID destination, Sensitivity sensitivity);
     List<NetworkPeer> getAllPeers(NetworkPeer fromPeer, int pageSize, int beginIndex);
-    Long totalPeers(NetworkPeer fromPeer, P2PRelationship.RelType relType);
+    Long totalPeersByRelationship(NetworkPeer fromPeer, P2PRelationship.RelType relType);
+    Long totalPeersByRelationshipAndSensitivity(NetworkPeer fromPeer, P2PRelationship.RelType relType, Sensitivity sensitivity);
     NetworkPeer getRandomPeer(NetworkPeer fromPeer);
+    NetworkPeer getRandomReachablePeer(NetworkPeer fromPeer, Sensitivity sensitivity);
     List<NetworkPeer> getReliablesToShare(NetworkPeer fromPeer);
     void reliablesFromRemotePeer(NetworkPeer remotePeer, List<NetworkPeer> reliables);
     Boolean savePeerStatusTimes(NetworkPeer fromPeer, NetworkPeer toPeer, Long sent, Long acknowledged);
