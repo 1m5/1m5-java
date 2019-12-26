4cb26d7c91aa48e8b8df0099ed76bb6258fc19f3
objectorange <objectorange@1m5.io>
2019-12-26 00:12:00 -0700

    place resources in separate class

---
+++ src/main/java/io/onemfive/Resources.java
@@ -0,0 +1,13 @@
+package io.onemfive;
+
+import java.net.URL;
+
+public class Resources {
+    public static final URL SYS_TRAY_ICON_BLUE = Resources.class.getResource("images/sys_tray_icon_blue.png");
+    public static final URL SYS_TRAY_ICON_GRAY = Resources.class.getResource("images/sys_tray_icon_gray.png");
+    public static final URL SYS_TRAY_ICON_GREEN = Resources.class.getResource("images/sys_tray_icon_green.png");
+    public static final URL SYS_TRAY_ICON_ORANGE = Resources.class.getResource("images/sys_tray_icon_orange.png");
+    public static final URL SYS_TRAY_ICON_RED = Resources.class.getResource("images/sys_tray_icon_red.png");
+    public static final URL SYS_TRAY_ICON_WHITE = Resources.class.getResource("images/sys_tray_icon_white.png");
+    public static final URL SYS_TRAY_ICON_YELLOW = Resources.class.getResource("images/sys_tray_icon_yellow.png");
+}
