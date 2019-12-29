46830132b64ef63751f43813714f544220e636f0
objectorange <objectorange@1m5.io>
2019-12-28 23:29:16 -0700

    change sensitivity sensor mappings

--- src/main/java/io/onemfive/data/Sensitivity.java
+++ src/main/java/io/onemfive/data/Sensitivity.java
@@ -5,7 +5,7 @@ public enum Sensitivity { // with default sensors chosen
     LOW, // HTTPS - MANCON 5
     MEDIUM, // Tor - MANCON 4
     HIGH, // I2P - MANCON 3
-    VERYHIGH, // I2P Delayed - MANCON 2
-    EXTREME, // 1DN - MANCON 1
+    VERYHIGH, // Radio (1DN) - MANCON 2
+    EXTREME, // LiFi (1DN) - MANCON 1
     NEO // Intelligent Combination of Tor, I2P, and 1DN - MANCON 0
 }
