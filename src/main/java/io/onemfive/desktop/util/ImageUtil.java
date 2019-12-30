fb3c7a3da24a8d1057c40fc7ef6ac623b4c80029
objectorange <objectorange@1m5.io>
2019-12-30 00:14:17 -0700

    add image utility

---
+++ src/main/java/io/onemfive/desktop/util/ImageUtil.java
@@ -0,0 +1,39 @@
+package io.onemfive.desktop.util;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+
+public class ImageUtil {
+    private static final Logger log = LoggerFactory.getLogger(ImageUtil.class);
+
+    public static final String REMOVE_ICON = "image-remove";
+
+    public static ImageView getImageViewById(String id) {
+        ImageView imageView = new ImageView();
+        imageView.setId(id);
+        return imageView;
+    }
+
+    public static Image getApplicationIconImage () {
+        return getImageByUrl("images/icon_white.png");
+    }
+
+    private static Image getImageByUrl(String url) {
+        return new Image(ImageUtil.class.getResourceAsStream(url));
+    }
+
+    private static ImageView getImageViewByUrl(String url) {
+        return new ImageView(getImageByUrl(url));
+    }
+
+    public static boolean isRetina() {
+        final GraphicsConfiguration gfxConfig = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+        final AffineTransform transform = gfxConfig.getDefaultTransform();
+        return !transform.isIdentity();
+    }
+}
