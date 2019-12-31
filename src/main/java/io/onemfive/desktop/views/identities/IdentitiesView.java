e0b1a43c8b7e074d18fdf671bdd64e6bc489f0bf
objectorange <objectorange@1m5.io>
2019-12-31 02:04:22 -0700

    add identities view

---
+++ src/main/java/io/onemfive/desktop/views/identities/IdentitiesView.java
@@ -0,0 +1,7 @@
+package io.onemfive.desktop.views.identities;
+
+import io.onemfive.desktop.views.InitializableView;
+import javafx.scene.control.TabPane;
+
+public class IdentitiesView extends InitializableView<TabPane, IdentitiesViewModel> {
+}
