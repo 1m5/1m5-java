c05640f910ea3ec05e983d630f1c83b3f43aeb17
objectorange <objectorange@1m5.io>
2019-12-31 02:03:16 -0700

    add dao view

---
+++ src/main/java/io/onemfive/desktop/views/dao/DaoView.java
@@ -0,0 +1,7 @@
+package io.onemfive.desktop.views.dao;
+
+import io.onemfive.desktop.views.InitializableView;
+import javafx.scene.control.TabPane;
+
+public class DaoView extends InitializableView<TabPane, DaoViewModel> {
+}
---
+++ src/main/java/io/onemfive/desktop/views/dao/DaoViewModel.java
@@ -0,0 +1,4 @@
+package io.onemfive.desktop.views.dao;
+
+public class DaoViewModel {
+}
