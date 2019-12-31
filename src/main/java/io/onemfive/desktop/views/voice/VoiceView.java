094214bb35ddf2fd04608251c375183304e6c9ec
objectorange <objectorange@1m5.io>
2019-12-31 02:05:37 -0700

    add voice view

---
+++ src/main/java/io/onemfive/desktop/views/voice/VoiceView.java
@@ -0,0 +1,7 @@
+package io.onemfive.desktop.views.voice;
+
+import io.onemfive.desktop.views.InitializableView;
+import javafx.scene.layout.HBox;
+
+public class VoiceView extends InitializableView<HBox, VoiceViewModel> {
+}
---
+++ src/main/java/io/onemfive/desktop/views/voice/VoiceViewModel.java
@@ -0,0 +1,4 @@
+package io.onemfive.desktop.views.voice;
+
+public class VoiceViewModel {
+}
