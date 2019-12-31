58c4c9ea0e54fd1114480be400e36e9669f3baee
objectorange <objectorange@1m5.io>
2019-12-31 02:03:00 -0700

    add calendar view

---
+++ src/main/java/io/onemfive/desktop/views/calendar/CalendarView.java
@@ -0,0 +1,7 @@
+package io.onemfive.desktop.views.calendar;
+
+import io.onemfive.desktop.views.InitializableView;
+import javafx.scene.layout.TilePane;
+
+public class CalendarView extends InitializableView<TilePane, CalendarViewModel> {
+}
---
+++ src/main/java/io/onemfive/desktop/views/calendar/CalendarViewModel.java
@@ -0,0 +1,4 @@
+package io.onemfive.desktop.views.calendar;
+
+public class CalendarViewModel {
+}
