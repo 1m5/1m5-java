b6ea3104d60cf225526544d9d035c88376acee7a
objectorange <objectorange@1m5.io>
2019-12-31 02:06:45 -0700

    build up main stage

--- src/main/java/io/onemfive/desktop/DesktopApp.java
+++ src/main/java/io/onemfive/desktop/DesktopApp.java
@@ -1,247 +1,107 @@
 package io.onemfive.desktop;
 
-import dorkbox.systemTray.*;
-import dorkbox.util.CacheUtil;
-import dorkbox.util.Desktop;
-import dorkbox.util.JavaFX;
-import dorkbox.util.OS;
+import dorkbox.systemTray.SystemTray;
 import io.onemfive.DAppTray;
-import io.onemfive.Resources;
 import io.onemfive.core.client.Client;
+import io.onemfive.core.util.AppThread;
+import io.onemfive.core.util.UncaughtExceptionHandler;
+import io.onemfive.desktop.util.CssTheme;
+import io.onemfive.desktop.util.ImageUtil;
+import io.onemfive.desktop.views.ViewLoader;
+import io.onemfive.desktop.views.home.HomeView;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.event.Event;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.util.Random;
+import java.awt.*;
+import java.util.logging.Logger;
 
-public class DesktopApp extends Application {
+import static io.onemfive.desktop.util.Layout.*;
+
+public class DesktopApp extends Application implements UncaughtExceptionHandler {
+
+    private static final Logger LOG = Logger.getLogger(DesktopApp.class.getName());
 
     private static Client client;
     private static DAppTray dAppTray;
     private static SystemTray systemTray;
+    private static AppThread routerThread;
+
     private static Runnable shutDownHandler;
 
-    private ActionListener callbackGray;
     private Stage stage;
     private boolean popupOpened;
     private Scene scene;
+    private boolean shutDownRequested;
 
-   public static void init(Client c, DAppTray tray) {
-       client = c;
-       dAppTray = tray;
-       systemTray = dAppTray.systemTray;
-       shutDownHandler = new Runnable() {
-           @Override
-           public void run() {
+    public DesktopApp() {
+        shutDownHandler = this::stop;
+    }
 
-           }
-       };
-   }
+    public static void setClient(Client c) {
+        client = c;
+    }
+
+    public static void setDappTray(DAppTray tray) {
+        dAppTray = tray;
+        systemTray = dAppTray.systemTray;
+    }
+
+    public static void setRouterThread(AppThread thread) {
+        routerThread = thread;
+    }
 
     @Override
-    public void start(Stage stage) throws Exception {
-       this.stage = stage;
-       stage.setTitle("1M5");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void init() throws Exception {
+        LOG.info("DesktopApp initializing...\n\tThread name: " + Thread.currentThread().getName());
+    }
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        stage.setScene(new Scene(root, 300, 250));
-        stage.show();
-
-        SystemTray.DEBUG = true; // for test apps, we always want to run in debug mode
-        CacheUtil.clear(); // for test apps, make sure the cache is always reset. You should never do this in production.
-
-//        SystemTray.APP_NAME = "1M5";
-
-        // SwingUtil.setLookAndFeel(null); // set Native L&F (this is the System L&F instead of CrossPlatform L&F)
-        // SystemTray.SWING_UI = new CustomSwingUI();
-
-        systemTray = SystemTray.get();
-        if (systemTray == null) {
-            throw new RuntimeException("Unable to load SystemTray!");
+    @Override
+    public void start(Stage stage) {
+        LOG.info("DesktopApp starting...\n\tThread name: " + Thread.currentThread().getName());
+        this.stage = stage;
+        stage.setTitle("1M5");
+        HomeView homeView = (HomeView) ViewLoader.load(HomeView.class);
+        Rectangle maxWindowBounds = new Rectangle();
+        try {
+            maxWindowBounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+        } catch (IllegalArgumentException e) {
+            // Multi-screen environments may encounter IllegalArgumentException (Window must not be zero)
+            // Just ignore the exception and continue, which means the window will use the minimum window size below
+            // since we are unable to determine if we can use a larger size
         }
+        scene = new Scene(homeView.getRoot(),
+                maxWindowBounds.width < INITIAL_WINDOW_WIDTH ?
+                        Math.max(maxWindowBounds.width, MIN_WINDOW_WIDTH) :
+                        INITIAL_WINDOW_WIDTH,
+                maxWindowBounds.height < INITIAL_WINDOW_HEIGHT ?
+                        Math.max(maxWindowBounds.height, MIN_WINDOW_HEIGHT) :
+                        INITIAL_WINDOW_HEIGHT);
 
-        systemTray.setTooltip("Mail Checker");
-        systemTray.setImage(Resources.SYS_TRAY_ICON_GRAY);
-        systemTray.setStatus("No Mail");
+        CssTheme.loadSceneStyles(scene, 1);
 
-        callbackGray = new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                final MenuItem entry = (MenuItem) e.getSource();
-                systemTray.setStatus(null);
-                systemTray.setImage(Resources.SYS_TRAY_ICON_WHITE);
+        // configure the system tray
+//        SystemTray.create(stage, shutDownHandler);
 
-                entry.setCallback(null);
-//                systemTray.setStatus("Mail Empty");
-                systemTray.getMenu().remove(entry);
-                System.err.println("POW");
-            }
-        };
+        stage.setOnCloseRequest(Event::consume);
 
+        // configure the primary stage
+        stage.setTitle("1M5");
+        stage.setScene(scene);
+        stage.setMinWidth(MIN_WINDOW_WIDTH);
+        stage.setMinHeight(MIN_WINDOW_HEIGHT);
+        stage.getIcons().add(ImageUtil.getApplicationIconImage());
 
-        Menu mainMenu = systemTray.getMenu();
-
-        MenuItem greenEntry = new MenuItem("Green Mail", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                final MenuItem entry = (MenuItem) e.getSource();
-                systemTray.setStatus("Some Mail!");
-                systemTray.setImage(Resources.SYS_TRAY_ICON_GREEN);
-
-                entry.setCallback(callbackGray);
-                entry.setImage(Resources.SYS_TRAY_ICON_WHITE);
-                entry.setText("Delete Mail");
-                entry.setTooltip(null); // remove the tooltip
-//                systemTray.remove(menuEntry);
-            }
-        });
-        greenEntry.setImage(Resources.SYS_TRAY_ICON_GREEN);
-        // case does not matter
-        greenEntry.setShortcut('G');
-        greenEntry.setTooltip("This means you have green mail!");
-        mainMenu.add(greenEntry);
-
-
-        Checkbox checkbox = new Checkbox("Euro € Mail", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                System.err.println("Am i checked? " + ((Checkbox) e.getSource()).getChecked());
-            }
-        });
-        checkbox.setShortcut('€');
-        mainMenu.add(checkbox);
-
-        MenuItem removeTest = new MenuItem("This should not be here", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                try {
-                    Desktop.browseURL("https://1m5.io");
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-        mainMenu.add(removeTest);
-        mainMenu.remove(removeTest);
-
-        mainMenu.add(new Separator());
-
-        mainMenu.add(new MenuItem("About", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                try {
-                    Desktop.browseURL("https://git.dorkbox.com/dorkbox/SystemTray");
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        }));
-
-        mainMenu.add(new MenuItem("Temp Directory", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                try {
-                    Desktop.browseDirectory(OS.TEMP_DIR.getAbsolutePath());
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        }));
-
-        Menu submenu = new Menu("Options", Resources.SYS_TRAY_ICON_BLUE);
-        submenu.setShortcut('t');
-
-
-        MenuItem disableMenu = new MenuItem("Disable menu", Resources.SYS_TRAY_ICON_WHITE, new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                MenuItem source = (MenuItem) e.getSource();
-                source.getParent().setEnabled(false);
-            }
-        });
-        submenu.add(disableMenu);
-
-
-        submenu.add(new MenuItem("Hide tray", Resources.SYS_TRAY_ICON_GRAY, new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                systemTray.setEnabled(false);
-            }
-        }));
-        submenu.add(new MenuItem("Remove menu", Resources.SYS_TRAY_ICON_WHITE, new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                MenuItem source = (MenuItem) e.getSource();
-                source.getParent().remove();
-            }
-        }));
-        submenu.add(new MenuItem("Add new entry to tray", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                systemTray.getMenu().add(new MenuItem("Random " + new Random().nextInt(10)));
-            }
-        }));
-        mainMenu.add(submenu);
-
-        MenuItem entry = new MenuItem("Type: ");
-        entry.setEnabled(false);
-        systemTray.getMenu().add(entry);
-
-        systemTray.getMenu().add(new MenuItem("Quit", new ActionListener() {
-            @Override
-            public
-            void actionPerformed(final java.awt.event.ActionEvent e) {
-                systemTray.shutdown();
-
-                if (!JavaFX.isEventThread()) {
-                    JavaFX.dispatch(new Runnable() {
-                        @Override
-                        public
-                        void run() {
-                            stage.hide(); // must do this BEFORE Platform.exit() otherwise odd errors show up
-                            Platform.exit();  // necessary to close javaFx
-                        }
-                    });
-                } else {
-                    stage.hide(); // must do this BEFORE Platform.exit() otherwise odd errors show up
-                    Platform.exit();  // necessary to close javaFx
-                }
-
-                //System.exit(0);  not necessary if all non-daemon threads have stopped.
-            }
-        })).setShortcut('q'); // case does not matter
+        // make the UI visible
+        stage.show();
     }
 
     @Override
     public void stop() {
-//        if (!shutDownRequested) {
-//            new Popup<>().headLine(Res.get("popup.shutDownInProgress.headline"))
+        if (!shutDownRequested) {
+//            new Popup().headLine(Res.get("popup.shutDownInProgress.headline"))
 //                    .backgroundInfo(Res.get("popup.shutDownInProgress.msg"))
 //                    .hideCloseButton()
 //                    .useAnimation(false)
@@ -251,8 +111,33 @@ public class DesktopApp extends Application {
 //                    log.debug("App shutdown complete");
 //                });
 //            }, 200, TimeUnit.MILLISECONDS);
-//            shutDownRequested = true;
-//        }
+            shutDownRequested = true;
+        }
     }
+
+    @Override
+    public void handleUncaughtException(Throwable throwable, boolean doShutDown) {
+        if (!shutDownRequested) {
+            if (scene == null) {
+                LOG.warning("Scene not available yet, we create a new scene. The bug might be caused by an exception in a constructor or by a circular dependency in Guice. throwable=" + throwable.toString());
+                scene = new Scene(new StackPane(), 1000, 650);
+                CssTheme.loadSceneStyles(scene, CssTheme.CSS_THEME_LIGHT);
+                stage.setScene(scene);
+                stage.show();
+            }
+            try {
+                if (doShutDown)
+                    stop();
+            } catch (Throwable throwable2) {
+                // If printStackTrace cause a further exception we don't pass the throwable to the Popup.
+                LOG.severe(throwable2.getLocalizedMessage());
+                stop();
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        launch(DesktopApp.class);
+//    }
 
 }
