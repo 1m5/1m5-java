742426dad88d27b60d300e02448c155b5937cded
objectorange <objectorange@1m5.io>
2019-12-31 02:03:35 -0700

    add dashboard view

---
+++ src/main/java/io/onemfive/desktop/views/dashboard/DashboardView.java
@@ -0,0 +1,8 @@
+package io.onemfive.desktop.views.dashboard;
+
+import io.onemfive.desktop.views.InitializableView;
+import javafx.scene.layout.StackPane;
+
+public class DashboardView extends InitializableView<StackPane, DashboardViewModel> {
+}
+
---
+++ src/main/java/io/onemfive/desktop/views/dashboard/DashboardViewModel.java
@@ -0,0 +1,4 @@
+package io.onemfive.desktop.views.dashboard;
+
+public class DashboardViewModel {
+}
