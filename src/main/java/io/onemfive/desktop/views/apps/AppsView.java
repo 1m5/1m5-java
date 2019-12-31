8be17c514673273d7aaef455f3aa01c30629be59
objectorange <objectorange@1m5.io>
2019-12-31 02:02:28 -0700

    add apps view

---
+++ src/main/java/io/onemfive/desktop/views/apps/AppsView.java
@@ -0,0 +1,8 @@
+package io.onemfive.desktop.views.apps;
+
+import io.onemfive.desktop.views.BaseView;
+import javafx.scene.layout.TilePane;
+
+public class AppsView extends BaseView<TilePane, AppsViewModel> {
+
+}
---
+++ src/main/java/io/onemfive/desktop/views/apps/AppsViewModel.java
@@ -0,0 +1,4 @@
+package io.onemfive.desktop.views.apps;
+
+public class AppsViewModel {
+}
