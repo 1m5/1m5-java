57b81c0295b40ea3fcb926365ab3283f0dcd7124
objectorange <objectorange@1m5.io>
2020-01-09 15:32:55 -0700

    update paths

--- src/main/java/io/onemfive/Resources.java
+++ src/main/java/io/onemfive/Resources.java
@@ -3,11 +3,11 @@ package io.onemfive;
 import java.net.URL;
 
 public class Resources {
-    public static final URL SYS_TRAY_ICON_BLUE = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_blue.png");
-    public static final URL SYS_TRAY_ICON_GRAY = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_gray.png");
-    public static final URL SYS_TRAY_ICON_GREEN = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_green.png");
-    public static final URL SYS_TRAY_ICON_ORANGE = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_orange.png");
-    public static final URL SYS_TRAY_ICON_RED = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_red.png");
-    public static final URL SYS_TRAY_ICON_WHITE = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_white.png");
-    public static final URL SYS_TRAY_ICON_YELLOW = Resources.class.getResource("/io/onemfive/images/sys_tray_icon_yellow.png");
+    public static final URL SYS_TRAY_ICON_BLUE = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_blue.png");
+    public static final URL SYS_TRAY_ICON_GRAY = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_gray.png");
+    public static final URL SYS_TRAY_ICON_GREEN = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_green.png");
+    public static final URL SYS_TRAY_ICON_ORANGE = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_orange.png");
+    public static final URL SYS_TRAY_ICON_RED = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_red.png");
+    public static final URL SYS_TRAY_ICON_WHITE = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_white.png");
+    public static final URL SYS_TRAY_ICON_YELLOW = Resources.class.getResource("/io/onemfive/desktop/images/sys_tray_icon_yellow.png");
 }
