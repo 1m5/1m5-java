cf232ec405e466b98a9d4c91f19ac8df82511a8e
objectorange <objectorange@1m5.io>
2020-01-11 10:16:11 -0700

    remove underscore

--- src/main/java/io/onemfive/util/ConcurrentHashSet.java
+++ src/main/java/io/onemfive/util/ConcurrentHashSet.java
@@ -38,53 +38,53 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ConcurrentHashSet<E> extends AbstractSet<E> implements Set<E> {
     private static final Object DUMMY = new Object();
-    private final Map<E, Object> _map;
+    private final Map<E, Object> map;
 
     public ConcurrentHashSet() {
-        _map = new ConcurrentHashMap<E, Object>();
+        map = new ConcurrentHashMap<E, Object>();
     }
     public ConcurrentHashSet(int capacity) {
-        _map = new ConcurrentHashMap<E, Object>(capacity);
+        map = new ConcurrentHashMap<E, Object>(capacity);
     }
 
     @Override
     public boolean add(E o) {
-        return _map.put(o, DUMMY) == null;
+        return map.put(o, DUMMY) == null;
     }
 
     @Override
     public void clear() {
-        _map.clear();
+        map.clear();
     }
 
     @Override
     public boolean contains(Object o) {
-        return _map.containsKey(o);
+        return map.containsKey(o);
     }
 
     @Override
     public boolean isEmpty() {
-        return _map.isEmpty();
+        return map.isEmpty();
     }
 
     @Override
     public boolean remove(Object o) {
-        return _map.remove(o) != null;
+        return map.remove(o) != null;
     }
 
     public int size() {
-        return _map.size();
+        return map.size();
     }
 
     public Iterator<E> iterator() {
-        return _map.keySet().iterator();
+        return map.keySet().iterator();
     }
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
         boolean rv = false;
         for (E e : c)
-            rv |= _map.put(e, DUMMY) == null;
+            rv |= map.put(e, DUMMY) == null;
         return rv;
     }
 }
