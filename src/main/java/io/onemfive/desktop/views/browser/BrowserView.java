f124b6b10f86142acf78e894e45ea67d78778529
objectorange <objectorange@1m5.io>
2020-01-13 14:31:15 -0700

    Enable browsing

--- src/main/java/io/onemfive/desktop/views/browser/BrowserView.java
+++ src/main/java/io/onemfive/desktop/views/browser/BrowserView.java
@@ -56,102 +56,102 @@ public class BrowserView extends InitializableView {
         LOG.info("Initializing...");
         super.initialize();
 
-//        BorderPane rootContainer = (BorderPane) root;
-//        engine.setJavaScriptEnabled(true);
-//
-//        VBox vBox = new VBox();
-//        vBox.setPadding(new Insets(5));
-//        vBox.setSpacing(5);
-//        rootContainer.setCenter(vBox);
-//
-//        HBox nav = new HBox();
-//        nav.setPadding(new Insets(5));
-//        nav.setSpacing(5);
-//        vBox.getChildren().add(nav);
-//
-//        TextField url = new TextField();
-//        url.setText("https://1m5.io");
-//        HBox.setHgrow(url, Priority.ALWAYS);
-//        nav.getChildren().add(url);
-//
-//        Button go = new Button("Go");
-//        go.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                LOG.info("Go: "+url.getText());
-//                engine.load(url.getText());
-//                LOG.info(url.getText()+" loaded");
-//            }
-//        });
-//        nav.getChildren().add(go);
-//
-//        Button refresh = new Button("Refresh");
-//        refresh.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                LOG.info("Refresh: "+url.getText());
-//                engine.reload();
-//                LOG.info(url.getText()+" resfreshed");
-//            }
-//        });
-//        nav.getChildren().add(refresh);
-//
-//        Button back = new Button("Back");
-//        back.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                LOG.info("Back...");
-//                history.go(-1);
-//                LOG.info("Backed");
-//            }
-//        });
-//        nav.getChildren().add(back);
-//
-//        Button stop = new Button("Stop");
-//        stop.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent actionEvent) {
-//                LOG.info("Cancel...");
-//                engine.getLoadWorker().cancel();
-//                LOG.info("Canceled");
-//            }
-//        });
-//        nav.getChildren().add(stop);
-//
-//        ScrollPane scrollPane = new ScrollPane();
-//        scrollPane.setContent(webView);
-//        // TODO: Change this to auto-fill space
-//        scrollPane.setFitToWidth(true);
-//        scrollPane.setFitToHeight(true);
-//        webView.setPrefWidth(1200);
-//
-//        vBox.getChildren().add(scrollPane);
-//
-//        engine.getLoadWorker().stateProperty()
-//                .addListener(new ChangeListener<Worker.State>() {
-//                    @Override
-//                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
-//                        if (newState == Worker.State.SUCCEEDED) {
-//                            url.setText(engine.getLocation());
-//                        }
-//                    }
-//                });
-//        engine.load("https://1m5.io");
-//
-//        history.getEntries().addListener(new ListChangeListener<WebHistory.Entry>() {
-//             @Override
-//             public void onChanged(Change<? extends WebHistory.Entry> c) {
-//                 c.next();
-//                 for (WebHistory.Entry e : c.getRemoved()) {
-//                     LOG.info(e.getUrl());
-//                 }
-//                 for (WebHistory.Entry e : c.getAddedSubList()) {
-//                     LOG.info(e.getUrl());
-//                 }
-//             }
-//            }
-//        );
-//        history.go(0);
+        BorderPane rootContainer = (BorderPane) root;
+        engine.setJavaScriptEnabled(true);
+
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(5));
+        vBox.setSpacing(5);
+        rootContainer.setCenter(vBox);
+
+        HBox nav = new HBox();
+        nav.setPadding(new Insets(5));
+        nav.setSpacing(5);
+        vBox.getChildren().add(nav);
+
+        TextField url = new TextField();
+        url.setText("https://1m5.io");
+        HBox.setHgrow(url, Priority.ALWAYS);
+        nav.getChildren().add(url);
+
+        Button go = new Button("Go");
+        go.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                LOG.info("Go: "+url.getText());
+                engine.load(url.getText());
+                LOG.info(url.getText()+" loaded");
+            }
+        });
+        nav.getChildren().add(go);
+
+        Button refresh = new Button("Refresh");
+        refresh.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                LOG.info("Refresh: "+url.getText());
+                engine.reload();
+                LOG.info(url.getText()+" resfreshed");
+            }
+        });
+        nav.getChildren().add(refresh);
+
+        Button back = new Button("Back");
+        back.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                LOG.info("Back...");
+                history.go(-1);
+                LOG.info("Backed");
+            }
+        });
+        nav.getChildren().add(back);
+
+        Button stop = new Button("Stop");
+        stop.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                LOG.info("Cancel...");
+                engine.getLoadWorker().cancel();
+                LOG.info("Canceled");
+            }
+        });
+        nav.getChildren().add(stop);
+
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setContent(webView);
+        // TODO: Change this to auto-fill space
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        webView.setPrefWidth(1200);
+
+        vBox.getChildren().add(scrollPane);
+
+        engine.getLoadWorker().stateProperty()
+                .addListener(new ChangeListener<Worker.State>() {
+                    @Override
+                    public void changed(ObservableValue ov, Worker.State oldState, Worker.State newState) {
+                        if (newState == Worker.State.SUCCEEDED) {
+                            url.setText(engine.getLocation());
+                        }
+                    }
+                });
+        engine.load("https://1m5.io");
+
+        history.getEntries().addListener(new ListChangeListener<WebHistory.Entry>() {
+             @Override
+             public void onChanged(Change<? extends WebHistory.Entry> c) {
+                 c.next();
+                 for (WebHistory.Entry e : c.getRemoved()) {
+                     LOG.info(e.getUrl());
+                 }
+                 for (WebHistory.Entry e : c.getAddedSubList()) {
+                     LOG.info(e.getUrl());
+                 }
+             }
+            }
+        );
+        history.go(0);
 
         LOG.info("Initialized.");
     }
