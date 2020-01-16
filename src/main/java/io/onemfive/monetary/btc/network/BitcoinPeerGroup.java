7637f8f830684230829df03884a1b3c0a4df0dfb
objectorange <objectorange@1m5.io>
2020-01-16 12:10:33 -0700

    add Bitcoin to name to differentiate from other discovery classes

--- src/main/java/io/onemfive/monetary/btc/network/BitcoinPeerGroup.java
+++ src/main/java/io/onemfive/monetary/btc/network/BitcoinPeerGroup.java
@@ -26,38 +26,10 @@
  */
 package io.onemfive.monetary.btc.network;
 
-import io.onemfive.monetary.btc.BitcoinContext;
-import io.onemfive.monetary.btc.blockchain.BlockChain;
-import io.onemfive.monetary.btc.wallet.Wallet;
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * TODO: Add Description
  *
  * @author objectorange
  */
-public class PeerDiscovery {
-
-    private BitcoinContext context;
-    private BlockChain chain;
-    private List<Wallet> wallets;
-
-    /**
-     * With no blockchain provided, height will appear at zero.
-     * Good for exploring the network without downloading blocks.
-     * @param context
-     */
-    public PeerDiscovery(BitcoinContext context) {
-        this(context, null);
-    }
-
-    public PeerDiscovery(BitcoinContext context, BlockChain chain) {
-        this.context = context;
-        this.chain = chain;
-        this.wallets = new ArrayList<>();
-    }
-
-
+public class BitcoinPeerGroup {
 }
