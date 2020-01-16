1b63d18c54a880bcb8f71c6feaf34a64d198f306
objectorange <objectorange@1m5.io>
2020-01-16 12:37:58 -0700

    fix mapping of Service Reports and describe class

--- src/main/java/io/onemfive/network/NetworkPeerReport.java
+++ src/main/java/io/onemfive/network/NetworkPeerReport.java
@@ -31,10 +31,15 @@ import io.onemfive.data.JSONSerializable;
 import io.onemfive.util.JSONParser;
 import io.onemfive.util.JSONPretty;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Information on the status of a Network Peer,
+ * its Services, and its access to the 1M5 Network.
+ */
 public class NetworkPeerReport implements JSONSerializable {
 
     public List<ServiceReport> serviceReports;
@@ -43,7 +48,13 @@ public class NetworkPeerReport implements JSONSerializable {
     @Override
     public Map<String, Object> toMap() {
         Map<String,Object> m = new HashMap<>();
-        if(serviceReports!=null) m.put("serviceReports", serviceReports);
+        if(serviceReports!=null) {
+            List<Map<String,Object>> lm = new ArrayList<>();
+            m.put("serviceReports", lm);
+            for(ServiceReport sr : serviceReports) {
+                lm.add(sr.toMap());
+            }
+        }
         if(networkReport!=null) m.put("networkReport", networkReport.toMap());
         return m;
     }
@@ -51,7 +62,14 @@ public class NetworkPeerReport implements JSONSerializable {
     @Override
     public void fromMap(Map<String, Object> m) {
         if(m.get("serviceReports")!=null) {
-            serviceReports = (List<ServiceReport>)m.get("serviceReports");
+            serviceReports = new ArrayList<>();
+            List<Map<String,Object>> lm = (List<Map<String,Object>>)m.get("serviceReports");
+            ServiceReport sr;
+            for(Map<String,Object> sm : lm) {
+                sr = new ServiceReport();
+                sr.fromMap(sm);
+                serviceReports.add(sr);
+            }
         }
         if(m.get("networkReport")!=null) {
             networkReport = new NetworkReport();
