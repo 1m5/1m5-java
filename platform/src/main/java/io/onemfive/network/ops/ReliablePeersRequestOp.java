292aa841d252c806e7034c9798281d1114da1be8
objectorange <objectorange@1m5.io>
2020-02-04 23:39:51 -0700

    Ask for reliable peers from a known peer.

---
+++ platform/src/main/java/io/onemfive/network/ops/ReliablePeersRequestOp.java
@@ -0,0 +1,50 @@
+/*
+  This is free and unencumbered software released into the public domain.
+
+  Anyone is free to copy, modify, publish, use, compile, sell, or
+  distribute this software, either in source code form or as a compiled
+  binary, for any purpose, commercial or non-commercial, and by any
+  means.
+
+  In jurisdictions that recognize copyright laws, the author or authors
+  of this software dedicate any and all copyright interest in the
+  software to the public domain. We make this dedication for the benefit
+  of the public at large and to the detriment of our heirs and
+  successors. We intend this dedication to be an overt act of
+  relinquishment in perpetuity of all present and future rights to this
+  software under copyright law.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+  OTHER DEALINGS IN THE SOFTWARE.
+
+  For more information, please refer to <http://unlicense.org/>
+ */
+package io.onemfive.network.ops;
+
+import io.onemfive.network.sensors.SensorManager;
+
+import java.util.logging.Logger;
+
+public class ReliablePeersRequestOp extends NetworkOpBase {
+
+    private static final Logger LOG = Logger.getLogger(ReliablePeersRequestOp.class.getName());
+
+    public ReliablePeersRequestOp(SensorManager sensorManager) {
+        super(sensorManager);
+    }
+
+    public OpsPacket operate(OpsPacket packet) {
+//        LOG.info("Received PeerStatus request...");
+//        peerManager.reliablesFromRemotePeer(request.getFromPeer(), request.getReliablePeers());
+//        request.setResponding(true);
+//        request.setReliablePeers(peerManager.getReliablesToShare(peerManager.getLocalPeer()));
+//        LOG.info("Sending response to PeerStatus request...");
+//        routeOut(new ResponsePacket(request, peerManager.getLocalPeer(), request.getFromPeer(), StatusCode.OK, request.getId()));
+        return packet;
+    }
+}
