066fffad9a8915c1405f047d5fd649fe883b186b
objectorange <objectorange@1m5.io>
2020-02-23 19:15:02 -0700

    update packaging

--- platform/src/main/c/GNURadio.c
+++ platform/src/main/c/GNURadio.c
@@ -2,7 +2,7 @@
 #include <std.io>
 #include "GNURadio.h"
 
-JNIEXPORT jint JNICALL Java_io_onemfive_radio_vendor_GNURadio_sendMessage(JNIEnv *, jobject, jbyteArray) {
+JNIEXPORT jint JNICALL Java_io_onemfive_network_sensors_fullspectrum_nets_gnu_GNURadioNet_sendMessage(JNIEnv *, jobject, jbyteArray) {
     printf("Message sent.")
     return 1;
 }
