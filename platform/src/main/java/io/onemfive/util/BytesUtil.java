472644efeb1cd19c413c1d5529613eab98e52491
objectorange <objectorange@1m5.io>
2020-03-20 10:47:20 -0700

    no need for BigInteger

--- platform/src/main/java/io/onemfive/util/BytesUtil.java
+++ platform/src/main/java/io/onemfive/util/BytesUtil.java
@@ -26,15 +26,37 @@
  */
 package io.onemfive.util;
 
-import java.math.BigInteger;
-
 public class BytesUtil {
 
-    public static int toInt(byte[] bytes) {
-        return new BigInteger(bytes).intValue();
+    public static int packBigEndian(byte[] b) {
+        return (b[0] & 0xFF) << 24
+                | (b[1] & 0xFF) << 16
+                | (b[2] & 0xFF) <<  8
+                | (b[3] & 0xFF);
     }
 
-    public static byte[] toBytes(int num) {
-        return BigInteger.valueOf(num).toByteArray();
+    public static byte[] unpackBigEndian(int x) {
+        return new byte[] {
+                (byte)(x >>> 24),
+                (byte)(x >>> 16),
+                (byte)(x >>>  8),
+                (byte)(x)
+        };
+    }
+
+    public static int packLittleEndian(byte[] b) {
+        return (b[0] & 0xFF)
+                | (b[1] & 0xFF) <<  8
+                | (b[2] & 0xFF) << 16
+                | (b[3] & 0xFF) << 24;
+    }
+
+    public static byte[] unpackLittleEndian(int x) {
+        return new byte[]{
+                (byte) (x),
+                (byte) (x >>> 8),
+                (byte) (x >>> 16),
+                (byte) (x >>> 24)
+        };
     }
 }
