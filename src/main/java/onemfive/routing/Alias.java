8c9bda2639f6c4e8c6ebda2dcaf3ac5fc09e662e
objectorange <brian@resolvingarchitecture.io>
2021-08-24 07:16:23 -0400

    moved neo4j into 1M5; upgraded RA components

---
+++ src/main/java/onemfive/routing/Alias.java
@@ -0,0 +1,7 @@
+package onemfive.routing;
+
+import org.neo4j.graphdb.RelationshipType;
+
+enum Alias implements RelationshipType {
+    Is
+}
---
+++ src/main/java/onemfive/routing/RelTypes.java
@@ -0,0 +1,7 @@
+package onemfive.routing;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public enum RelTypes implements RelationshipType {
+    KNOWS
+}
